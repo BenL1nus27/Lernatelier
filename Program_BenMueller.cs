0c195b155903af463f6d6f482bc57a979387daed
BenL1nus27 <111043950+BenL1nus27@users.noreply.github.com>
2022-09-08 20:16:29 +0200

    Add files via upload

--- Program_BenMueller.cs
+++ Program_BenMueller.cs
@@ -3,10 +3,11 @@ using System.Globalization;
 
 namespace RandomNumberGenerator
 {
-    internal class Program
+    internal class programm
     {
-        static void Main(string[] args)
+        static void _Main(string[] args)
         {
+
             Console.WriteLine("");
             Console.WriteLine("░▒█▀▀▄░█▀▀▄░█▀▀▄░█▀▄░▄▀▀▄░█▀▄▀█░░░▒█▄░▒█░█░▒█░█▀▄▀█░█▀▀▄░█▀▀░█▀▀▄░░░▒█▀▀█░█▀▀░█▀▀▄░█▀▀░█▀▀▄░█▀▀▄░▀█▀░▄▀▀▄░█▀▀▄\r\n░▒█▄▄▀░█▄▄█░█░▒█░█░█░█░░█░█░▀░█░░░▒█▒█▒█░█░▒█░█░▀░█░█▀▀▄░█▀▀░█▄▄▀░░░▒█░▄▄░█▀▀░█░▒█░█▀▀░█▄▄▀░█▄▄█░░█░░█░░█░█▄▄▀\r\n░▒█░▒█░▀░░▀░▀░░▀░▀▀░░░▀▀░░▀░░▒▀░░░▒█░░▀█░░▀▀▀░▀░░▒▀░▀▀▀▀░▀▀▀░▀░▀▀░░░▒█▄▄▀░▀▀▀░▀░░▀░▀▀▀░▀░▀▀░▀░░▀░░▀░░░▀▀░░▀░▀▀\r\n");
             Console.WriteLine("");
@@ -21,7 +22,7 @@ namespace RandomNumberGenerator
                 {
 
 
-                    Antwort=Convert.ToBoolean(Console.ReadLine());
+                    Antwort = Convert.ToBoolean(Console.ReadLine());
                     anfang = true;
                 }
                 catch
@@ -37,8 +38,8 @@ namespace RandomNumberGenerator
                 Console.WriteLine("Ich werde dir mit Tipps helfen:)");
                 Console.WriteLine("");
 
-               //NumberGen#1 für iene Zahl zwischen 1 und 101
-                
+                //NumberGen#1 für iene Zahl zwischen 1 und 101
+
                 Random rnd = new Random();
                 int rndNum = rnd.Next(1, 101);
                 int i = 0;
@@ -105,81 +106,81 @@ namespace RandomNumberGenerator
 
                         }
                     }
-                }    
-                    
+                }
+
                 //NumberGen#2 für Zahl zwischen 1 und 1001
-                    Random rnd2 = new Random();
-                    int rndNum2 = rnd2.Next(1, 1001);
-                    int y = 0;
-                    bool resultFound2 = false;
+                Random rnd2 = new Random();
+                int rndNum2 = rnd2.Next(1, 1001);
+                int y = 0;
+                bool resultFound2 = false;
 
-                    while (!resultFound)
+                while (!resultFound)
+                {
+                    int num2 = 0;
+
+                    try
                     {
-                        int num2 = 0;
+                        Console.Write("Zahl:");
+                        num2 = int.Parse(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Deine eingegebende Zahl ist keine Ganzahl!");
 
-                        try
-                        {
-                            Console.Write("Zahl:");
-                            num2 = int.Parse(Console.ReadLine());
-                        }
-                        catch
-                        {
-                            Console.WriteLine("Deine eingegebende Zahl ist keine Ganzahl!");
+                        Console.WriteLine("------------------------------------------");
+                        Main(args);
+                        break;
 
-                            Console.WriteLine("------------------------------------------");
-                            Main(args);
-                            break;
-
-                        }
+                    }
 
 
-                        if (num2 > rndNum2)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("Die gesuchte Zahl ist tiefer");
-                            y++;
-                            Console.WriteLine("------------------------------------------");
-                            Console.ResetColor();
-                        }
+                    if (num2 > rndNum2)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Die gesuchte Zahl ist tiefer");
+                        y++;
+                        Console.WriteLine("------------------------------------------");
+                        Console.ResetColor();
+                    }
 
-                        else if (num2 < rndNum2)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("Die gesuchte Zahl ist höher");
-                            y++;
-                           
-                            Console.WriteLine("------------------------------------------");
-                            Console.ResetColor();
-                        }
+                    else if (num2 < rndNum2)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Die gesuchte Zahl ist höher");
+                        y++;
 
-                        else if (Antwort == false)
-                        {
-                            Console.WriteLine("Schade Marmelade ");
+                        Console.WriteLine("------------------------------------------");
+                        Console.ResetColor();
+                    }
 
-                        }
+                    else if (Antwort == false)
+                    {
+                        Console.WriteLine("Schade Marmelade ");
 
-                        else
-                        {
-                            Console.ForegroundColor = ConsoleColor.Yellow;
-                            Console.Write("Super! Du hast die gesuchte Zahl in ");
-                            Console.Write(y);
-                            Console.Write(" Versuchen gefunden");
-                            Console.ResetColor();
-                            resultFound2 = true;
-                            y++;
+                    }
 
-
-                        }
-
-
-
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write("Super! Du hast die gesuchte Zahl in ");
+                        Console.Write(y);
+                        Console.Write(" Versuchen gefunden");
+                        Console.ResetColor();
+                        resultFound2 = true;
+                        y++;
 
 
                     }
 
 
 
+
+
                 }
+
+
+
             }
         }
     }
+}
